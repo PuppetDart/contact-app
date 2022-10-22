c4d71c247972afd779d05fcfcb19ad23e3499df5
Abhishek Pandey <chiseldart@hotmail.com>
2022-10-22 15:31:19 +0530

    genral commit

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkCPkcOvS_0GgRqT3AqzpHQ-1GrP9fMSU",
+  authDomain: "contactapp-puppetdart.firebaseapp.com",
+  projectId: "contactapp-puppetdart",
+  storageBucket: "contactapp-puppetdart.appspot.com",
+  messagingSenderId: "569406780549",
+  appId: "1:569406780549:web:404500060c1d4bb4bee5ec",
+  measurementId: "G-23R248LV0T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
